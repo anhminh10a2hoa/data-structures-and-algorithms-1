83e0bcb173fa187a9b283414bc151aeda826eda0
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-11-02 12:08:06 +0200

    minValue

--- wk09/improving_functions/improve2.cc
+++ wk09/improving_functions/improve2.cc
@@ -10,11 +10,9 @@
  * @return int - the smallest value or 0 (zero) if vector is empty.
  */
 int minValue(std::vector<int> vec){
-    if ( vec.empty() ) { return 0; }
-    if ( std::is_sorted(vec.begin(), vec.end()) ) {
-        return vec[0];
+    if (vec.empty()) {
+        return 0;
     } else {
-        std::sort(vec.begin(), vec.end());
-        return vec[0];
+        return *std::min_element(vec.begin(), vec.end());
     }
 }
